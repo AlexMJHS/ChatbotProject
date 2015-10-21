154a75b06472d2b22c5297a8c7056aa06f9c1967
Alejandro <AlexMJHS@gmail.com>
2015-10-21 09:10:36 -0600

    Added the ChatView and fixed error.

---
+++ src/chat/view/ChatView.java
@@ -0,0 +1,18 @@
+package chat.view;
+
+import javax.swing.JOptionPane;
+
+public class ChatView 
+{
+	public void showResonse(String wordsFromSomewhere)
+	{
+		JOptionPane.showMessageDialog(null, wordsFromSomewhere);
+	}
+	
+	public String grabAnswer(String popupText)
+	{
+		String answer = "";
+		answer = JOptionPane.showInputDialog(null, popupText);
+		return answer;
+	}
+}
