129187a6068a34c8f071b850928efde502eae360
Alejandro <AlexMJHS@gmail.com>
2016-03-07 09:15:22 -0700

    Added sendTweet method

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -6,7 +6,7 @@ import twitter4j.*;
 /**
  * 
  * @author Alejandro Magallanes
- * @version 0.1
+ * @version 0.2 Ref to Twitter4j
  * Will interact with Twitter
  *
  */
@@ -16,4 +16,15 @@ public class CTECTwitter
 	//Declaration
 	private ArrayList<Status> statusList;
 	private ArrayList<String> wordList;
+	
+	public CTECTwitter()
+	{
+		statusList = new ArrayList<Status>();
+		wordList = new ArrayList<String>();
+	}
+	
+	public void sendTweet()
+	{
+		chatbotTwitter.updateStatus("I just tweeted from my Java Chatbot program! #APCSRocks @CTECNow Thanks @cscheerleader & @codyhenrichsen!");
+	}
 }
