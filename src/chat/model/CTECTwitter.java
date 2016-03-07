57676e13bd327f8f5031dd2ae4b03bc3f3c08b32
Alejandro <AlexMJHS@gmail.com>
2016-03-07 08:57:30 -0700

    Declared ArrayLIst and added imports

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -1,6 +1,19 @@
 package chat.model;
 
+import java.util.ArrayList;
+import twitter4j.*;
+
+/**
+ * 
+ * @author Alejandro Magallanes
+ * @version 0.1
+ * Will interact with Twitter
+ *
+ */
+
 public class CTECTwitter
 {
-
+	//Declaration
+	private ArrayList<Status> statusList;
+	private ArrayList<String> wordList;
 }
